2cc25cd34cd248ae2d299d68b2483e4691afd385
Evan Horsley <evy0311@gmail.com>
2021-09-08 22:53:12 -0400

    Updating packages

--- src/react/Dashboard.js
+++ src/react/Dashboard.js
@@ -1,2 +1,12 @@
 import React, {Component} from 'react';
 import './Dashboard.css';
+
+export default function Dashboard() {
+
+
+    return (
+        <div>
+            TEST
+        </div>
+    );
+}
