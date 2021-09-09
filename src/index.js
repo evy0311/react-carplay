fcdd6093ebc73497db0ddead762018a7942c6ffd
Evan Horsley <evy0311@gmail.com>
2021-09-09 00:52:53 -0400

    Working on dashboard

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './react/App';
 
+import Dashboard from './react/Dashboard';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Dashboard/>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/react/Dashboard.js
+++ src/react/Dashboard.js
@@ -1,12 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Dashboard.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 export default function Dashboard() {
 
 
     return (
-        <div>
-            TEST
+        <div className="main">
+
+            {/* The active "tab" */}
+            <div className="active">
+
+            </div>
+
+            {/* Bottom navigation menu */}
+            <div className="nav">
+                <ButtonGroup aria-label="Basic example">
+                    <Button variant="primary" size="lg">CarPlay</Button>{' '}
+                    <Button variant="secondary"size="lg">Radio</Button>{' '}
+                    <Button variant="secondary"size="lg">Climate</Button>{' '}
+                    <Button variant="secondary"size="lg">Settings</Button>
+                </ButtonGroup>
+            </div>
         </div>
     );
 }
